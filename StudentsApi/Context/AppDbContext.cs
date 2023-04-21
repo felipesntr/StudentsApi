f6aa1a55b8f1cf6e2523ffdc67d7f108c5fb37dd
Felipe Santos <rfelipe273@gmail.com>
2023-04-21 08:00:26 -0300

    Add project files.

---
+++ StudentsApi/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsApi.Models;
+
+namespace StudentsApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                    new Student { Id = 1, Name = "John", Age = 18, Email = "john@doe.com" },
+                    new Student
+                    {
+                        Id = 2,
+                        Name = "Jane",
+                        Age = 20,
+                        Email = "jane@email.com"
+                    }
+                );
+        }
+    }
+}
---
+++ StudentsApi/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentsApi.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80, MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
